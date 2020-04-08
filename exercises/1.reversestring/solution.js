ceaf3a7882869ba67b00fc82a1a38d9bcdd6f14c
savinaxu <jia_shijie@163.com>
2020-04-07 20:46:51 -0700

    upload interview files.

---
+++ exercises/1.reversestring/solution.js
@@ -0,0 +1,51 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// Class example
+function reverse(str) {
+    // string.split :The
+    const arr = str.split("") // ["T", "h", "e"] 0, 1, 2
+    const changedArr = []
+    for (let i = arr.length-1; i >= 0; i--) {
+        // i = arr.length-1-1 : arr[i] = "h"
+        changedArr.push(arr[i]) // ["e", "h", "T"]
+    }
+    // ["e", "h", "T"]
+    return changedArr.join("")
+
+
+}
+
+// solution 1
+function reverse(str) {
+    let result = ""
+    // The
+    for (let i = str.length-1; i >= 0; i--) {
+        result += str[i]
+    }
+    return result
+}
+
+//solution 2
+function reverse(str) {
+    return str.split("").reverse().join("")
+}
+
+//solution 3
+function reverse(str) {
+    let result = ""
+    for (let char of str) {
+        result = char + result
+    }
+    return result
+}
+
+//solution 4
+function reverse(str) {
+    return str.split("").reduce((rev, char) => char + rev, "")
+}
---
+++ exercises/18.linkedlist/index.js
@@ -0,0 +1,57 @@
+// --- Directions
+// Implement classes Node and Linked Lists
+// See 'directions' document
+
+class Node {
+    constructor(data, next=null) {
+    }
+}
+
+class LinkedList {
+    constructor() {
+    }
+
+    insertFirst(data) {
+    }
+
+    size() {
+    }
+
+    getFirst() {
+    }
+
+    getLast() {
+    }
+
+    clear() {
+    }
+
+    removeFirst() {
+    }
+
+    removeLast() {
+    }
+
+    insertLast(data) {
+    }
+
+    getAt(integer) {
+    }
+
+    removeAt(integer) {
+    }
+
+    insertAt(data, integer) {
+    }
+
+    forEach(fn) {
+    }
+
+    *[Symbol.iterator]() {
+    }
+
+
+
+}
+
+module.exports = { Node, LinkedList };
